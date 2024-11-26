67443b7956ebd698a322c9045ba4bc0b8bb68a3e
Yulian47 <78698459+Yulian47@users.noreply.github.com>
2024-11-26 14:35:01 +0100

    add task solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,48 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  let result = 0;
+  const calculator = {
+    operate(callback, number) {
+      if (typeof callback === 'function') {
+        result = callback(result, number);
+      }
+
+      return this;
+    },
+
+    getResult() {
+      return result;
+    },
+
+    add(a, b) {
+      return a + b;
+    },
+
+    subtract(a, b) {
+      return a - b;
+    },
+
+    multiply(a, b) {
+      return a * b;
+    },
+
+    divide(a, b) {
+      if (b !== 0) {
+        return a / b;
+      } else {
+        return a;
+      }
+    },
+
+    reset() {
+      result = 0;
+
+      return this;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
